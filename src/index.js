9d732fc873641561c1e3d6c7c460c26bd6df9238
EgorRybin <antoniopareiro@gmail.com>
2022-08-29 22:34:36 +0300

    update js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from '../src/js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+const input = document.querySelector('#search-box');
+const ul = document.querySelector('.country-list');
+const div = document.querySelector('.country-info');
+
+input.addEventListener('input', onInput);
+
+function onInput(evt) {
+  console.log(fetchCountries(evt.target.value));
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,13 @@
+
+export const fetchCountries = (name) => {
+  return fetch(`https://restcountries.com/v2/name/${name}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(country => {
+      return country;
+    });
+};
